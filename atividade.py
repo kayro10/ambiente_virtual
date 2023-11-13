54c73fdb47303723b7c1f5438259f9f68283f34b
kayro10 <145942819+kayro10@users.noreply.github.com>
2023-11-13 13:41:42 -0300

    atividade incompleta do arq aula11.09

---
+++ atividade.py
@@ -0,0 +1,16 @@
+# Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimentos e empresas.
+# Ambiente que proporciona conexão, troca de conhecimentos e aprendizado 
+# Compartilhado entre participantes, instrutores e empresas parceiras
+
+texto =" Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimentos e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(texto[0:5])
+print(texto[10:16])
+print(texto[20:30])
+print(texto[34:44])
+print(texto[49:53])
+print(texto[54:60])
+print(texto[67:74])
+print(texto[76:89])
+print(texto[92:100])
+print(texto[102:110])
---
+++ atividade02.py
@@ -0,0 +1,17 @@
+# 1. Faça o fatiamento de 10 palavras do texto abaixo
+# 2. Faça o fatiamento de 8 palavras e monte uma frasa com a impressão dessas palavras.
+# [i:f:p]  
+
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimentos e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(texto[0:5])
+print(texto[10:16])
+print(texto[20:30])
+print(texto[34:44])
+print(texto[49:53])
+print(texto[54:60])
+print(texto[67:74])
+print(texto[76:89])
+print(texto[92:100])
+print(texto[102:110])
+
